48a85522c51733d35e089d8732485511376fa01c
Brian Voter <bvoter97@gmail.com>
2023-05-17 14:36:35 -0400

    fixes config test and adds docstring for sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -2,8 +2,25 @@
 
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
-
+/**
+ * @param {object} dataToUpdate object mapping keys to vals that will be set
+ * in the database  E.G. { firstName: 'Aliya', age: 32 }
+ *
+ * @param {object} jsToSql object mapping certain keys in dataToUpdate to their
+ * corresponding column names in the database E.G. { firstName: "first_name" }
+ *
+ * @returns {object} object with setCols and values. setCols is a string
+ * for the SET clause of the DB query like "column_1=$1, column_2=$2, ..."
+ * E.G. " "first_name"=$1, "age"=$2 "
+ *
+ * values is an array of values corresponding to the column names in
+ * setCols E.G. ['Aliya', 32]
+ *
+ * format: {
+ *  setCols,
+ *  values
+ * }
+ */
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
